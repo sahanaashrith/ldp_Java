25286a78af636beeb1e5ac87056a3f890147e126
sahanaashrith <99783811+sahanaashrith@users.noreply.github.com>
2022-03-15 17:33:57 +0530

    Create AllAlphabets.java

---
+++ assignment2/AllAlphabets.java
@@ -0,0 +1,24 @@
+//Write a java function that checks if the input string contains all the letters of the alphabet a-z (case-insensitive).
+
+import java.util.Scanner;
+
+public class AllAlphabets {
+       public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter the string: ");
+        String str = myObj.nextLine();       // read input
+        str = str.toLowerCase();
+
+        boolean success = true;
+        for(char c = 'a';c <= 'z'; ++c) {
+            if(!str.contains(String.valueOf(c))) {
+                success = false;
+                break;
+            }
+        }
+        if (success)
+            System.out.println("String contains all alphabets");
+        else
+            System.out.println("String DOESN'T contains all alphabets");
+    }
+}
